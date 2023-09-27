1f974b669f301e2822a164fb679c0df8b7e8ab3d
Shuhei Ota <shuhei.ota.so@gmail.com>
2023-09-26 22:14:22 -0700

    0915done

---
+++ 0915_tasks/script.js
@@ -0,0 +1,31 @@
+/**************************CODING TASK*********************************
+
+1. Create variables to represent scores of exams for Matt and Simon and set values 80 and 45;
+2. Create variable to set the lower limit for passing an exam and assign to it value as 51;
+3. Write the condition in if statement, which will check if both students have passed an exam and log
+   the result in console;
+4. Use else if statement in order to check if one of the students has passed an exam and log the result
+   in console;
+5. Use else statement in order to display in console that both students have failed;
+6. In case of passing the exam by one of the students, find out which one was that and display in console
+   as an additional information the student name and his points
+7. Test all the possible cases (a. both students passed; b. Matt passed and Simon failed; c. Simon passed and
+  Matt failed; d. both students failed).
+*/
+   
+// const passScore = 60;
+// const scoreMatt = 60;
+// const scoreSimon = 74;
+// if (scoreMatt >= passScore && scoreSimon >= passScore) {
+//   console.log('Both students have passed');
+// } else if(scoreMatt < passScore && scoreSimon > passScore) {
+//   console.log(`Matt has failed. Score: ${scoreMatt}`);
+//   } else if(scoreMatt > passScore && scoreSimon < passScore) {
+//    console.log(`Simon has failed. Score: ${scoreSimon}`);
+//   } else {
+//   console.log('Both has failed');
+// };
+
+let num = 7.959;
+let finalNum = Math.round(num * 100) / 100 ;
+console.log(finalNum);
